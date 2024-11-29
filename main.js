a30846089cb0b3903b940232c4ee8f1fbc2bed24
mrhl082024 <mrhl082024@gmail.com>
2024-11-29 10:34:22 +0100

    gjort oppgave 1,2,3 og 4

--- main.js
+++ main.js
@@ -1,15 +1,66 @@
 //1. create an array of 5 person objects, the objhects should contain first name last name, age and job properties, jobb should be a boolean.
+
+let person = [
+  {
+    firstName: "Lars Henrik",
+    lastName: "Roan Midtsæter",
+    age: "24",
+    job: false,
+  },
+  {
+    firstName: "Bax",
+    lastName: "Fyyy",
+    age: "69",
+    job: false,
+  },
+  {
+    firstName: "Lars",
+    lastName: "Krogsrud",
+    age: "13",
+    job: true,
+  },
+  {
+    firstName: "Marius",
+    lastName: "Aasen",
+    age: "24",
+    job: false,
+  },
+  {
+    firstName: "Jesper",
+    lastName: "Lotterud",
+    age: "24",
+    job: true,
+  },
+  ,
+];
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 //2. print First and last name of the first person in the array. using dot notation on firstname and bracket notation last name
+
+const firstPerson = person[0];
+console.log(firstPerson.firstName + " " + firstPerson["lastName"]);
+
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 //3. That was tiresome.. just so much typing. Lets write a method to that we never need to that again..
 //create a method in every person object that returns first and last name, call it fullName. This can be done manually for each one or with a loop.
 //Print fullName of the second person in the array by calling the method.
+person.forEach((e) => {
+  e.fullName = function fullName() {
+    return this.firstName + " " + this.lastName;
+  };
+});
+
+console.log(person[1].fullName());
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 //4. Its the third person's birthday! And their job status changed. Update their age and job status using dot notation.
+
+person[2].age = 14;
+person[2].job = false;
+
+console.log(person[2]);
+
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
 //5. Person three is throwing a giant party! create a function called fotballPubben(). The function should check if the person is over 18, print "party time" if they are and "too young" if they are not. It should also print the name of the person.
